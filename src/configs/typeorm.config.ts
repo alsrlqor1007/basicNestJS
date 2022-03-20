ceeb8ab708a6098b2e0bf9c2eca5dea36ae507da
alsrlqor <skyup9393@gmail.com>
2022-03-20 23:11:16 +0900

    Updated config

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -7,9 +7,9 @@ export const typeORMConfig : TypeOrmModuleOptions = {
     port: 5432,
     username: 'postgres',
     password: 'postgres',
-    database: 'BoardProject',
+    database: 'board-app',
     entities: [__dirname + `/../**/*.entity.{js,ts}`],
-    // entitiy를 이용해 데이터베이스 테이블을 생성하기 때문에 entitiy 파일 위치를 설정해준다.
+    // entity를 이용해 데이터베이스 테이블을 생성하기 때문에 entitiy 파일 위치를 설정해준다.
     synchronize: true
-    // synchronize: true는 애플리케이션을 재실행할 때 테이블을 Drop한 후, entitiy 안에서 수정된 컬럼의 길이, 타입, 변경 값 등을 다시 생성해준다.
+    // synchronize: true는 애플리케이션을 재실행할 때 테이블을 Drop한 후, entity 안에서 수정된 컬럼의 길이, 타입, 변경 값 등을 다시 생성해준다.
 }
